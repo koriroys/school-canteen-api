7d4ea5b725e95cab6bf732cd4354eecaa4f8a575
Kori Roys <kori@koriroys.com>
2016-12-21 19:17:06 +0100

    basic user model

--- config/test.exs
+++ config/test.exs
@@ -12,8 +12,6 @@ config :logger, level: :warn
 # Configure your database
 config :school_canteen, SchoolCanteen.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
   database: "school_canteen_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
