8e228812cacdc29c4f1e498474b09f49b6705284
Kori Roys <kori@koriroys.com>
2016-12-28 03:03:04 +0100

    refactor

--- web/controllers/admin_user_controller.ex
+++ web/controllers/admin_user_controller.ex
@@ -10,16 +10,14 @@ defmodule SchoolCanteen.AdminUserController do
     admin_user = conn
     |> Guardian.Plug.current_resource
 
-    if is_admin(admin_user) do
-      conn
-      |> render(SchoolCanteen.AdminUserView, "show.json", admin_user: admin_user)
-    else
-      conn
-      |> put_status(403)
-      |> render(SchoolCanteen.ErrorView, "403.json")
+    case admin_user do
+      %AdminUser{} ->
+        conn
+        |> render(SchoolCanteen.AdminUserView, "show.json", admin_user: admin_user)
+      _ ->
+        conn
+        |> put_status(403)
+        |> render(SchoolCanteen.ErrorView, "403.json")
     end
   end
-
-  def is_admin(%AdminUser{}), do: true
-  def is_admin(_), do: false
 end
